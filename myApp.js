2fa5cbc3756011581f6c994df4daf3e5e7fa8432
Alexander Luna <alexfox_2000@hotmail.com>
2023-02-11 00:32:25 -0500

    implement #findOneByFood

--- myApp.js
+++ myApp.js
@@ -37,7 +37,11 @@ const findPeopleByName = (personName, done) => {
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (err, data) => {
+    if (err == null) {
+      done(null, data);
+    }
+  });
 };
 
 const findPersonById = (personId, done) => {
